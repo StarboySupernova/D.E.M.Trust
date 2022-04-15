b15087abfbbd1e07922db34c27a18b759da52ce8
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-04-15 02:00:35 +0100

    Initial Commit

---
+++ D.E.M.Trust/D_E_M_TrustApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_E_M_TrustApp.swift
+//  D.E.M.Trust
+//
+//  Created by user217235 on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct D_E_M_TrustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
